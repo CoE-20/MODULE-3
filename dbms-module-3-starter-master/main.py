68f230f2b0b8c57995ca04abd51f656c828a17ff
Joanna <jm.ellarina@gmail.com>
2015-08-14 00:41:44 -0700

    Updated Module 3

--- dbms-module-3-starter-master/main.py
+++ dbms-module-3-starter-master/main.py
@@ -5,13 +5,18 @@ import os
 import logging
 import json
 
-
-
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+class Thesis(ndb.Model):
+    year = ndb.IntegerProperty()
+    Title = ndb.StringProperty(indexed=True)
+    abstract = ndb.TextProperty(indexed=True)
+    adviser = ndb.StringProperty()
+    section = ndb.IntegerProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
 
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
@@ -19,7 +24,54 @@ class MainPageHandler(webapp2.RequestHandler):
         self.response.write(template.render())
 
 
+class APIStudentHandler(webapp2.RequestHandler):
+    def get(self):
+        details = Thesis.query().order(-Thesis.date).fetch()
+        detail_list = []
+
+        for detail in details:
+            detail_list.append({
+                'id': detail.key.urlsafe(),
+                'year': detail.year,
+                'Title': detail.Title,
+                'abstract': detail.abstract,
+                'adviser': detail.adviser,
+                'section': detail.section
+            })
+
+        response = {
+            'result': 'OK',
+            'data': detail_list
+        }
+        self.response.headers['Content-Type'] = 'application.json'        
+        self.response.out.write(json.dumps(response))
+
+    def post(self):
+        detail = Thesis()
+        detail.year = int(self.request.get('year'))
+        detail.Title = self.request.get('Title')
+        detail.abstract = self.request.get('abstract')
+        detail.adviser = self.request.get('adviser')
+        detail.section = int(self.request.get('section'))
+        detail.put()
+
+        self.response.headers['Content-Type'] = 'application/json'
+        response = {
+            'result': 'OK',
+            'data': {
+                'id': detail.key.urlsafe(),
+                'year': detail.year,
+                'Title': detail.Title,
+                'abstract': detail.abstract,
+                'adviser': detail.adviser,
+                'section': detail.section,
+            }
+        }
+        self.response.headers['Content-Type'] = 'application/json'        
+        self.response.out.write(json.dumps(response))
+  
+
 app = webapp2.WSGIApplication([
-    ('/home', MainPageHandler),
+    ('/api/thesis', APIStudentHandler),
     ('/', MainPageHandler)
 ], debug=True)
--- dbms-module-3-starter-master/static/script.js
+++ dbms-module-3-starter-master/static/script.js
@@ -1,5 +1,39 @@
-(function($) {
+function onFormSubmit(event){
+    var data = $(event.target).serializeArray();
+    var thesis = {};
+    for (var i = 0; i<data.length; i++)
+    {
+        thesis[data[i].name] = data[i].value
+    }
 
-    // your code here
+    var thesis_create_api = "/api/thesis";
+    $.post(thesis_create_api, thesis, function(response){
+        if (response.status = 'OK'){
+            var full_details = response.data.year+ ' ' +response.data.Title+ ' ' +response.data.abstract+ ' ' +response.data.adviser+ ' ' +response.data.adviser 
+            + ' ' +response.data.section
+             $('#thesis-list').append('<li>' + full_details + '</li>')
+        }else{
 
-})(jQuery)
+        }
+    });
+    
+    return false;
+}
+
+function loadAllThesisdetails(){
+    var student_list_api = '/api/thesis';
+    $.get(student_list_api, {}, function(response){
+        console.log('thesis-list', response)
+        response.data.forEach(function(thesis) {
+            var full_details = thesis.year + ' ' + thesis.Title + ' ' + thesis.abstract + ' ' + thesis.adviser + ' ' + thesis.section
+        $('#thesis-list').append('<li>' + full_details + '</li>')    
+        });
+    });
+}
+
+
+$('#create-form').submit(onFormSubmit)
+loadAllThesisdetails();
+$('#create-form').submit(function(onFormSubmit){
+    (this).reset()
+});
